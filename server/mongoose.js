8463e0f4650a37480606bd40f2b2f843ec7ae8af
Siddharth <siddharthdongre06@gmail.com>
2023-09-02 14:04:02 +0530

    rename node file to express in server side

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,37 +1,37 @@
 import mongoose from "mongoose";
 
 // Mongoose Connection
-mongoose.connect("mongodb://localhost:27017/sidd", {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
+mongoose.connect("mongodb://127.0.0.1:27017/MyMeals", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 
 db.on("error", () => {
-    console.error.bind(console, "Connection error");
+  console.error.bind(console, "Connection error");
 });
 
 db.once("open", () => {
-    console.log("We are connected");
+  console.log("We are connected");
 });
 
 // Mongoose schema, model and document for model collection
 const schemaModel = new mongoose.Schema({
-    name : String,
-    email : String,
-    password : String,
-    reEnterPassword : String
+  name: String,
+  email: String,
+  password: String,
+  reEnterPassword: String
 });
 
 export const documentModel = mongoose.model("model", schemaModel);
 
 // Mongoose schema, model and document for model collection
 const schemaContact = new mongoose.Schema({
-    name : String,
-    email : String,
-    phone : String,
-    message : String
+  name: String,
+  email: String,
+  phone: String,
+  message: String
 });
 
 export const documentContact = mongoose.model("contact", schemaContact);
--- server/router.js
+++ server/router.js
@@ -1,70 +1,63 @@
 import { documentModel, documentContact } from "./mongoose.js";
 
 export const contact = (req, res) => {
-    const { name, email, phone, message } = req.body;
+  const { name, email, phone, message } = req.body;
 
-    documentContact.insertMany({
-        name : name,
-        email : email,
-        phone : phone,
-        message : message 
-    }, (err, data) => {
-        if(err){
-            res.send({ message : "Some error occured" });
-        }
-        else{
-            res.send({ message : "Message successfully submitted", data : data });
-        }
-    })
+  documentContact.insertMany({
+    name: name,
+    email: email,
+    phone: phone,
+    message: message
+  }, (err, data) => {
+    if (err) {
+      res.send({ message: "Some error occured" });
+    } else {
+      res.send({ message: "Message successfully submitted", data: data });
+    }
+  })
 };
 
-export const signup =  (req, res) => {
-    const { name, email, password, reEnterPassword } = req.body;
+export const signup = (req, res) => {
+  const { name, email, password, reEnterPassword } = req.body;
 
-    documentModel.findOne({email : email}, (err, data) => {
-        if(err){
-            res.send({ message : "Some error occured", err : err });
-        }
-        else if(data){
-            res.send({ message : "User already registered" });
-        }
-        else {
-            documentModel.insertMany({
-                name : name,
-                email : email,
-                password : password,
-                reEnterPassword : reEnterPassword
-            }, (err, data) => {
-                if(err){
-                    console.log(err);
-                    res.send({ message : "Some error occured", err : err });
-                }
-                else{
-                    console.log(data);
-                    res.send({ message : "Successfully Registered", data : data });
-                };
-            })
+  documentModel.findOne({ email: email }, (err, data) => {
+    if (err) {
+      res.send({ message: "Some error occured", err: err });
+    } else if (data) {
+      res.send({ message: "User already registered" });
+    } else {
+      documentModel.insertMany({
+        name: name,
+        email: email,
+        password: password,
+        reEnterPassword: reEnterPassword
+      }, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.send({ message: "Some error occured", err: err });
+        } else {
+          console.log(data);
+          res.send({ message: "Successfully Registered", data: data });
         };
-    })
+      })
+    };
+  })
 };
 
 export const login = (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    documentModel.findOne({email : email}, (err, data) => {
-        if(err){
-            res.send({ message : "Some error occured", err : err });
-        }
-        else if(data){
-            if(password === data.password){
-                res.send({ message : "Login Successfully", data : data });
-            }
-            else {
-                res.send({ message : "Password did not matched" });
-            }
-        }
-        else {
-            res.send({ message : "User not registered" });
-        }
-    })
+  documentModel.findOne({ email: email }, (err, data) => {
+    if (err) {
+      res.send({ message: "Some error occured", err: err });
+    } else if (data) {
+      if (password === data.password) {
+        res.send({ message: "Login Successfully", data: data });
+      } else {
+        res.send({ message: "Password did not matched" });
+      }
+    } else {
+      res.send({ message: "User not registered" });
+    };
+  });
 };
